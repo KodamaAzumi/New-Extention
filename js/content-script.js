7c0b9e8d31bbcff6c77ad3185185a639191b0af7
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2022-07-18 19:04:57 +0900

    #右揃え、画像上下反転

--- js/content-script.js
+++ js/content-script.js
@@ -7,9 +7,21 @@ window.addEventListener('scroll', function(){
     console.log("縦スクロール：" + window.scrollY);
 
     //下にスクロールするほどグレースケールになる
-    //this.document.body.style.filter = `grayscale(${window.scrollY/10}%)`;
+    let gs = window.scrollY/10;
+    console.log(gs);
+    this.document.body.style.filter = `grayscale(${Math.max(gs, 0)}%)`;
 
     //下にスクロールするほど不透明度が下がる
-    this.document.body.style.opacity =`${(1-(window.scrollY/1000)*0.5)}`
+    let op = 1-(window.scrollY/1000)*0.5;
+    console.log(op);
+    this.document.body.style.opacity =`${Math.max(op, 0)}`
 });
+
+document.body.style.textAlign = 'right';
+
+const imglist = document.querySelectorAll('img');
+imglist.forEach((img) =>{
+    img.style.transform = 'scale(-1,-1)';
+});
+
 
--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,11 @@
 const gsOffbutton = document.getElementById('gsOff')
 
 let gsOff = () =>{
+    console.log('gsOffが押されました');
+    /*
     document.body.style.filter = 'grayscale(0%)';
     chrome.tabs.reload();
+    */
 };
 
 gsOffbutton.addEventListener('click', gsOff)
@@ -11,8 +14,10 @@ const opOffbutton = document.getElementById('opOff')
 
 let opOff = () =>{
     console.log('opOffが押されました');
+    /*
     document.body.style.opacity = '1';
     chrome.tabs.reload();
+    */
 };
 
 opOffbutton.addEventListener('click', opOff)
