6844ccd48fc5aef81987850f1e19056081cdb062
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2022-07-19 17:39:30 +0900

    #p5.jsのライブラリを追加して、丸を描画できるようになった

--- js/content-script.js
+++ js/content-script.js
@@ -1,3 +1,4 @@
+/*
 window.addEventListener('load', function(){
     console.log('ページが読み込まれました');
 });
@@ -17,11 +18,52 @@ window.addEventListener('scroll', function(){
     this.document.body.style.opacity =`${Math.max(op, 0)}`
 });
 
+//右揃え
 document.body.style.textAlign = 'right';
 
+//画像を反転させる
 const imglist = document.querySelectorAll('img');
 imglist.forEach((img) =>{
     img.style.transform = 'scale(-1,-1)';
 });
+
+*/
+
+//縦書き
+//document.body.style.writingMode= 'vertical-rl';
+
+
+
+function setup() {
+  document.body.insertAdjacentHTML('afterbegin', '<div id="p5Canvas"></div>');
+  let canvas = createCanvas(1920, 1000);
+  canvas.parent('p5Canvas');
+
+  // 背景色の設定
+  background('rgba(0, 0, 0, 0)');
+
+/*
+  window.addEventListener('beforeunload', (event) =>{
+    saveCanvas(canvas, 'myCanvas', 'png');
+    //event.preventDefault();
+    console.log('beforeunloadが機能している');
+  });
+*/
+}
+
+function draw() {
+  noStroke();
+  fill('rgba(0, 0, 0, 0.2)');
+  circle(mouseX, mouseY, 5);
+ /*
+  stroke('rgba(0, 0, 0, 0.2)');
+  strokeWeight(2);
+  noFill();
+  line(mouseX, mouseY, pmouseX, pmouseY)
+  */
+}
+
+
+
 
 
